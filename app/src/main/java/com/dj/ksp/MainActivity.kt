fc0ccbb0de9f906eaba3c62ebdd1d5c43e4bb614
daniyaljavaid <daniyaljavaid95@gmail.com>
2023-07-06 11:47:46 +0500

    inject annotation added

--- app/src/main/java/com/dj/ksp/MainActivity.kt
+++ app/src/main/java/com/dj/ksp/MainActivity.kt
@@ -4,11 +4,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.dj.app.R
-import com.dj.testannotation.RepositoryAnnotation
-import com.dj.testannotation.ViewModelAnnotation
+import com.dj.ksp.repository.IRepository
+import com.dj.ksp.usecase.IUseCase
+import com.dj.ksp.viewmodel.IViewModel
 import generated.file.printHackFunction
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var repository: IRepository
+
+    @Inject
+    lateinit var usecase: IUseCase
+
+    @Inject
+    lateinit var viewmodel: IViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
